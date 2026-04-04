fc3876f5818398d24e2bc4dc62dad6143d18fc10
shunter09 <sumirahhunter@gmail.com>
2026-04-04 18:17:05 -0400

    Commit

---
+++ src/MovieCard.jsx
@@ -0,0 +1,21 @@
+function MovieCard({movie}) {
+    function onFavoriteClick() {
+        alert("clicked")
+    }
+    return <div className="movie-card">
+        <div className="movie_poster">
+            <img src={movie.url} alt={movie.title}/>
+            <div className="movie-overlay">
+                <button className="favorite-btn" onClick={onFavoriteClick}>👍</button>
+            </div>
+        </div>
+        <div className="movie-info">
+            <h2>{movie.title}</h2>
+            <img src={movie.imageUrl} alt={movie.title} />
+            <p>{movie.release_date}</p>
+
+        </div>
+    </div>
+}
+
+export default MovieCard;
